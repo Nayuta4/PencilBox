adebc4b6188640ac0e9a130b84e5274656628006
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2007-01-20 09:52:18 +0000

    Modified NewBoardDialog class.

--- pencilbox/trunk/src/pencilbox/common/gui/NewBoardDialog.java
+++ pencilbox/trunk/src/pencilbox/common/gui/NewBoardDialog.java
@@ -1,24 +1,29 @@
 package pencilbox.common.gui;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 
 import pencilbox.common.core.Size;
-import pencilbox.common.factory.PencilBoxClassException;
-import pencilbox.common.factory.PencilType;
-
 
 /**
- * 「新規」ダイアログ
+ * 「新規作成」ダイアログ
  * モーダルダイアログなので，1つのインスタンスのみを生成して使い回す
  */
 
 public class NewBoardDialog extends PencilBoxDialog {
-	
+
 	private static NewBoardDialog instance = new NewBoardDialog();
 
 	/**
@@ -29,14 +34,11 @@ public class NewBoardDialog extends PencilBoxDialog {
 	public static NewBoardDialog getInstance() {
 		return instance;
 	}
-	
-	private JPanel freeSizePanel;
-	private JTextField fieldRows;
-	private JTextField fieldCols;
 
-//	private boolean squareOnly = false;
-	private boolean freeSize = true;
-	private Size newSize;
+	private JPanel mainPanel;
+	private JSpinner spinnerRows;
+	private JSpinner spinnerCols;
+	private JCheckBox checkBox;
 
 	private NewBoardDialog() {
 		super();
@@ -44,78 +46,80 @@ public class NewBoardDialog extends PencilBoxDialog {
 
 	protected void makeDialog() {
 		super.makeDialog();
-		makeFreeSizePanel();
+		makeMainPanel();
 	}
 	
-	private void makeFreeSizePanel() {
-		freeSizePanel = new JPanel();
-		freeSizePanel.setLayout(new FlowLayout());
-		fieldCols = new JTextField(3);
-		fieldRows = new JTextField(3);
-		freeSizePanel.add(new JLabel("ヨコ"));
-		freeSizePanel.add(fieldCols);
-		freeSizePanel.add(new JLabel("タテ"));
-		freeSizePanel.add(fieldRows);
-		this.add(freeSizePanel, BorderLayout.CENTER); 
+	private void makeMainPanel() {
+		mainPanel = new JPanel(new GridBagLayout());
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+		spinnerCols = new JSpinner();
+		spinnerRows = new JSpinner();
+		final SpinnerModel spinnerModelCols = new SpinnerNumberModel(10, 1, 200, 1);
+		final SpinnerModel spinnerModelRows = new SpinnerNumberModel(10, 1, 200, 1);
+		spinnerCols.setModel(spinnerModelCols);
+		spinnerRows.setModel(spinnerModelRows);
+		JLabel labelCols = new JLabel("ヨコ： ");
+		JLabel labelRows = new JLabel("タテ： ");
+		labelCols.setHorizontalAlignment(SwingConstants.TRAILING);
+		labelRows.setHorizontalAlignment(SwingConstants.TRAILING);
+
+		checkBox = new JCheckBox("正方形");
+		checkBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (checkBox.isSelected()) {
+					spinnerRows.setModel(spinnerModelCols);
+//					spinnerRows.setEnabled(false);
+				} else {
+					spinnerModelRows.setValue(spinnerModelCols.getValue());
+					spinnerRows.setModel(spinnerModelRows);
+//					spinnerRows.setEnabled(true);
+				}
+			}
+		});
+
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.insets.top = 2;
+		constraints.insets.bottom = 2;
+		constraints.insets.left = 0;
+		constraints.insets.right = 0;
+		constraints.fill = GridBagConstraints.BOTH; 
+		
+		addComponent(labelCols,   constraints, 0, 0, 1, 1);
+		addComponent(spinnerCols, constraints, 1, 0, 1, 1);
+		addComponent(labelRows,   constraints, 0, 1, 1, 1);
+		addComponent(spinnerRows, constraints, 1, 1, 1, 1);
+		addComponent(checkBox,    constraints, 1, 2, 1, 1);
+		this.add(mainPanel, BorderLayout.CENTER); 
+	}
+
+	private void addComponent(Component c, GridBagConstraints constraints, int x, int y, int w, int h) {
+		constraints.gridx = x;
+		constraints.gridy = y;
+		constraints.gridwidth = w;
+		constraints.gridheight = h;
+		mainPanel.add(c, constraints);
 	}
 
 	/**
-	 * パズルの種類を設定する
-	 * @param pencilType パズルの種類
-	 * @throws PencilBoxClassException
-	 */
-	public void setPencilType(PencilType pencilType) throws PencilBoxClassException {
-//		PuzzleCommon puzzleCommon = (PuzzleCommon) ClassUtil.createInstance(pencilType , "Puzzle");
-//		squareOnly = puzzleCommon.isSquarePuzzle();
-//		if (squareOnly) {
-//			fieldRows.setEditable(false);
-//			fieldRows.setFocusable(false);
-//		} else {
-//			fieldRows.setEditable(true);
-//			fieldRows.setFocusable(true);
-//		}
-	}
-	/**
-	 * 現在の盤面サイズの値を，ダイアログの自由設定サイズのフィールドに値を設定する
+	 * 現在の盤面サイズの値をダイアログに設定する
 	 * @param s 現在の盤面サイズ
 	 */
 	public void setCurrentSize(Size s) {
-		newSize = null;
-		if (freeSize) {
-			fieldCols.setText(Integer.toString(s.getCols()));
-			fieldCols.selectAll();
-			fieldRows.setText(Integer.toString(s.getRows()));
-			fieldRows.selectAll();
-		}
+		if (s.getCols() != s.getRows())
+			checkBox.setSelected(false);
+		spinnerCols.setValue(Integer.valueOf(s.getCols()));
+		spinnerRows.setValue(Integer.valueOf(s.getRows()));
 	}
 
-	protected void setInitialFocus() {
-		fieldCols.requestFocusInWindow();
-	}
-	
-	protected void accept() {
-		try {
-			int rows = Integer.parseInt(fieldRows.getText());
-			int cols = Integer.parseInt(fieldCols.getText());
-//			if (squareOnly) {
-//				rows = cols;
-//			}
-			newSize = new Size(rows, cols);
-		} catch (NumberFormatException e) {
-			showErrorMessage(e.toString());
-		} finally {
-			super.accept();
-		}
-	}
-	private void showErrorMessage(String message) {
-		JOptionPane.showMessageDialog(this,message,"Error",JOptionPane.ERROR_MESSAGE);
-	}
 	/**
-	 * ダイアログで選択されたサイズを取得する
-	 * @return ダイアログで選択されたサイズ
+	 * ダイアログに入力されたサイズを取得する
+	 * @return ダイアログに入力されたサイズ
 	 */
 	public Size getNewSize() {
-		return newSize;
+		int rows = ((Number) spinnerRows.getValue()).intValue();
+		int cols = ((Number) spinnerCols.getValue()).intValue();
+		return new Size(rows, cols);
 	}
-	
+
 }
