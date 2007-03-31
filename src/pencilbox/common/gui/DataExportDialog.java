60be24afaeca81d39686ca46bf41a0ef5e5ef8a4
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2007-03-31 14:07:42 +0000

    Changed DataExportdialog to enable import problem data.

--- src/pencilbox/common/gui/DataExportDialog.java
+++ src/pencilbox/common/gui/DataExportDialog.java
@@ -4,16 +4,32 @@
 package pencilbox.common.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 
 /**
  * 
  */
-public class DataExportDialog extends PencilBoxDialog {
+public class DataExportDialog extends JPanel {
 	
 	private JTextArea textArea;
 
@@ -22,12 +38,13 @@ public class DataExportDialog extends PencilBoxDialog {
 	 */
 	public DataExportDialog() {
 		super();
-		// TODO Auto-generated constructor stub
+		makeDialog();
 	}
 
 	protected void makeDialog() {
-	    setDialogType(OK_ONLY);
-		super.makeDialog();
+		this.setLayout(new BorderLayout());
+		makeButtonPanel();
+		assignKeys();
 		makeMainPanel();
 	}
 
@@ -47,4 +64,103 @@ public class DataExportDialog extends PencilBoxDialog {
 		textArea.setSelectionStart(0);
 		textArea.setSelectionEnd(s.length());
 	}
+	
+	public String getText() {
+		return textArea.getText();
+	}
+
+	private JPanel buttonPanel;
+	private JButton	buttonOpen;
+	private JButton buttonCancel;
+	private JButton buttonCopy;
+	private JDialog dialog;
+	private int ret = PencilBoxDialog.CLOSED_OPTION; // ダイアログの返り値
+
+	private void makeButtonPanel() {
+		buttonPanel = new JPanel();
+		buttonOpen = new JButton("問題を別フレームに開く");
+		buttonCopy = new JButton("クリップボードにコピーする");
+		buttonCancel = new JButton("閉じる");
+		ActionListener buttonAction = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Object source = e.getSource();
+				if (source == buttonOpen) {
+					open();
+				} else if (source == buttonCancel) {
+					cancel();
+				} else if (source == buttonCopy) {
+					copyToClipboard();
+				}
+			}
+		};
+		buttonOpen.addActionListener(buttonAction);
+		buttonCancel.addActionListener(buttonAction);
+		buttonCopy.addActionListener(buttonAction);
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(buttonCancel);
+		buttonPanel.add(buttonOpen);
+		buttonPanel.add(buttonCopy);
+		this.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * ESC キーでダイアログを閉じる
+	 */
+	private void assignKeys() {
+		InputMap imap = this.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		imap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "close");
+		ActionMap amap = this.getActionMap();
+		amap.put("close", new AbstractAction() {	
+			public void actionPerformed(ActionEvent e) {
+				if (dialog != null) { // 念のため
+					ret = PencilBoxDialog.CLOSED_OPTION;
+					dialog.setVisible(false);	
+				}
+			}
+		});
+	}
+
+	protected void open() {
+		ret = PencilBoxDialog.OK_OPTION;
+		dialog.setVisible(false);
+	}
+
+	protected void cancel() {
+		ret = PencilBoxDialog.CANCEL_OPTION;
+		dialog.setVisible(false);
+	}
+	
+	protected void copyToClipboard() {
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		StringSelection str = new StringSelection(getText());
+		clipboard.setContents(str , null);
+	}
+	
+	/**
+	 * ダイアログを表示する。
+	 * 毎回JDialogを作り直して，PencilBoxPaneをJDialogの中に入れて表示する。
+	 * @param parent 親フレーム
+	 * @param title タイトル文字列
+	 * @return ユーザーの選択した結果
+	 */
+//	参考 Core JAVA Vol.1 list 9-18
+	public int showDialog(Component parent, String title) {
+		Frame owner = null;
+		if (parent instanceof Frame)
+			owner = (Frame)parent;
+		else
+			owner = (Frame)SwingUtilities.getAncestorOfClass(Frame.class, parent);
+//      必要に応じて作り直す方式でフォーカスを毎回設定し直すやり方が分からないので，毎回作り直す
+//		if (dialog == null || dialog.getOwner() != owner) {
+			dialog = new JDialog(owner, true);
+			dialog.getContentPane().add(this);
+			dialog.pack();
+			dialog.getRootPane().setDefaultButton(buttonCancel);
+//		}
+		dialog.setTitle(title);
+		dialog.setLocationRelativeTo(owner);
+		dialog.setVisible(true);
+		return ret;
+	}
+
 }
