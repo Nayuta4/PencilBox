e5df72e00f32627f5f9ed7fb9186da0a31528dbe
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2007-05-04 07:25:33 +0000

    Use TreeMapA class to order preferences keys when storing preferences.

--- src/pencilbox/common/gui/PreferencesCopierBase.java
+++ src/pencilbox/common/gui/PreferencesCopierBase.java
@@ -4,11 +4,17 @@
 package pencilbox.common.gui;
 
 import java.awt.Color;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
 import pencilbox.common.factory.ClassUtil;
@@ -121,8 +127,11 @@ public class PreferencesCopierBase {
 				properties.load(new FileInputStream(file));
 			}
 			acquireCurrentPreferences(command);
-			properties.store(new FileOutputStream(file), "PencilBox preferences");
 //			properties.list(System.out);
+//			properties.store(new FileOutputStream(file), "PencilBox preferences");
+			// キーで整列するために，Properties を　TreeMap　に変換してから保存する。
+			TreeMapA map = new TreeMapA(properties);
+			map.store(new FileOutputStream(file), "PencilBox preferences");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -170,4 +179,123 @@ public class PreferencesCopierBase {
 		return String.format("0x%06X", new Object[] {Integer.valueOf(color.getRGB() & 0xFFFFFF)});
 	}
 
+}
+
+/**
+ * java.util.TreeMap クラスに java.util.Properties クラスと同様の store() メソッドを追加したもの。
+ */
+class TreeMapA extends java.util.TreeMap {
+
+    /**
+     * @param m
+     */
+    public TreeMapA(Map m) {
+        super(m);
+    }
+
+	/**
+	 * Map を ファイルに書き出す。
+	 * java.util.Properties クラスの　store()を真似た。
+	 * @param out
+	 * @param comments
+	 * @throws IOException
+	 */
+	public void store(OutputStream out, String comments) throws IOException {
+        BufferedWriter awriter;
+        awriter = new BufferedWriter(new OutputStreamWriter(out, "8859_1"));
+        if (comments != null)
+            writeln(awriter, "#" + comments);
+        writeln(awriter, "#" + new Date().toString());
+        for (Iterator e = keySet().iterator(); e.hasNext();) {
+            String key = (String)e.next();
+            String val = (String)get(key);
+            key = saveConvert(key, true);
+
+	    /* No need to escape embedded and trailing spaces for value, hence
+	     * pass false to flag.
+	     */
+            val = saveConvert(val, false);
+            writeln(awriter, key + "=" + val);
+        }
+        awriter.flush();
+    }
+
+    private static void writeln(BufferedWriter bw, String s) throws IOException {
+        bw.write(s);
+        bw.newLine();
+    }
+
+    /*
+     * Converts unicodes to encoded &#92;uxxxx and escapes
+     * special characters with a preceding slash
+     */
+    private String saveConvert(String theString, boolean escapeSpace) {
+        int len = theString.length();
+        int bufLen = len * 2;
+        if (bufLen < 0) {
+            bufLen = Integer.MAX_VALUE;
+        }
+        StringBuffer outBuffer = new StringBuffer(bufLen);
+
+        for(int x=0; x<len; x++) {
+            char aChar = theString.charAt(x);
+            // Handle common case first, selecting largest block that
+            // avoids the specials below
+            if ((aChar > 61) && (aChar < 127)) {
+                if (aChar == '\\') {
+                    outBuffer.append('\\'); outBuffer.append('\\');
+                    continue;
+                }
+                outBuffer.append(aChar);
+                continue;
+            }
+            switch(aChar) {
+		case ' ':
+		    if (x == 0 || escapeSpace) 
+			outBuffer.append('\\');
+		    outBuffer.append(' ');
+		    break;
+                case '\t':outBuffer.append('\\'); outBuffer.append('t');
+                          break;
+                case '\n':outBuffer.append('\\'); outBuffer.append('n');
+                          break;
+                case '\r':outBuffer.append('\\'); outBuffer.append('r');
+                          break;
+                case '\f':outBuffer.append('\\'); outBuffer.append('f');
+                          break;
+                case '=': // Fall through
+                case ':': // Fall through
+                case '#': // Fall through
+                case '!':
+                    outBuffer.append('\\'); outBuffer.append(aChar);
+                    break;
+                default:
+                    if ((aChar < 0x0020) || (aChar > 0x007e)) {
+                        outBuffer.append('\\');
+                        outBuffer.append('u');
+                        outBuffer.append(toHex((aChar >> 12) & 0xF));
+                        outBuffer.append(toHex((aChar >>  8) & 0xF));
+                        outBuffer.append(toHex((aChar >>  4) & 0xF));
+                        outBuffer.append(toHex( aChar        & 0xF));
+                    } else {
+                        outBuffer.append(aChar);
+                    }
+            }
+        }
+        return outBuffer.toString();
+    }
+
+    /**
+     * Convert a nibble to a hex character
+     * @param	nibble	the nibble to convert.
+     */
+    private static char toHex(int nibble) {
+	return hexDigit[(nibble & 0xF)];
+    }
+
+    /** A table of hex digits */
+    private static final char[] hexDigit = {
+	'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+    };
+	
 }
