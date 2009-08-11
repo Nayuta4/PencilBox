909069d75f3e7e7f314010cdc45b5b9e06e0f7de
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2009-08-11 05:19:06 +0000

    Update version number.

--- pencilbox/trunk/src/pencilbox/common/factory/Constants.java
+++ pencilbox/trunk/src/pencilbox/common/factory/Constants.java
@@ -5,8 +5,8 @@ package pencilbox.common.factory;
 public class Constants {
 	
 	public static final String TITLE = "PencilBox";
-	public static final String VERSION = "1.3.0";
-	public static final String COPYRIGHT = "Copyright (c) 2006-2007 Nayuta";
+	public static final String VERSION = "1.4.0";
+	public static final String COPYRIGHT = "Copyright (c) 2006-2009 Nayuta";
 	public static final String URL = "http://pencilbox.sourceforge.jp/";
 	public static final String ROOT_PACKAGE_NAME = "pencilbox";
 
