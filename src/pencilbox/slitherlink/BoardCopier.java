c86059e565545e06f3c1e98554a296e1b80f684e
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2010-04-03 11:30:11 +0000

    Remove Address.set from some BoardCopier.

--- src/pencilbox/slitherlink/BoardCopier.java
+++ src/pencilbox/slitherlink/BoardCopier.java
@@ -25,59 +25,46 @@ public class BoardCopier extends BoardCopierBase {
 	public void copyRegion(BoardBase srcBoardBase, BoardBase dstBoardBase, Area region, Address from, Address to, int rotation) {
 		Board srcBoard = (Board) srcBoardBase;
 		Board board = (Board) dstBoardBase;
-		Address dn = Address.address();
-		Address rt = Address.address();
-		Address dnrt = Address.address();
-		int joint;
-		int grid;
-		int dir;
 		for (Address s : region) {
 			Address d = translateAndRotateAddress(s, from, to, rotation);
-			dn.set(s.r()+1, s.c());
-			rt.set(s.r(), s.c()+1);
-			dnrt.set(s.r()+1, s.c()+1);
-			if (region.contains(dn)) {
-				joint = srcBoard.getStateJ(s, Direction.DN);
-				dir = Rotator2.rotateDirection(Direction.DN, rotation);
-				if (board.isSideOn(d, dir))
-					board.setStateJ(d, dir, joint);
+			for (int n : Direction.DN_RT) {
+				if (region.contains(Address.nextCell(s, n))) {
+					int joint = srcBoard.getStateJ(s, n);
+					int dir = Rotator2.rotateDirection(n, rotation);
+					if (board.isSideOn(d, dir))
+						board.setStateJ(d, dir, joint);
+				}
 			}
-			if (region.contains(rt)) {
-				joint = srcBoard.getStateJ(s, Direction.RT);
-				dir = Rotator2.rotateDirection(Direction.RT, rotation);
-				if (board.isSideOn(d, dir))
-					board.setStateJ(d, dir, joint);
-			}
+			Address dn = Address.nextCell(s, Direction.DN);
+			Address rt = Address.nextCell(s, Direction.RT);
+			Address dnrt = Address.nextCell(dn, Direction.RT);
 			if (region.containsAll(dn, rt, dnrt)) {
-				grid = srcBoard.getNumber(s);
+				int grid = srcBoard.getNumber(s);
+				Address dd = null;
 				if (rotation == 0 || rotation == 4)
-					d.set(d.r(), d.c());
+					dd = d;
 				else if (rotation == 1 || rotation == 7)
-					d.set(d.r()-1, d.c());
+					dd = Address.address(d.r()-1, d.c());
 				else if (rotation == 2 || rotation == 6)
-					d.set(d.r()-1, d.c()-1);
+					dd = Address.address(d.r()-1, d.c()-1);
 				else if (rotation == 3 || rotation == 5)
-					d.set(d.r(), d.c()-1);
-				if (board.isNumberOn(d))
-					board.setNumber(d, grid);
+					dd = Address.address(d.r(), d.c()-1);
+				if (board.isNumberOn(dd))
+					board.setNumber(dd, grid);
 			}
 		}
 	}
 
 	public void eraseRegion(BoardBase boardBase, Area region) {
 		Board board = (Board) boardBase;
-		Address dn = Address.address();
-		Address rt = Address.address();
-		Address dnrt = Address.address();
 		for (Address s : region) {
-			dn.set(s.r()+1, s.c());
-			rt.set(s.r(), s.c()+1);
-			dnrt.set(s.r()+1, s.c()+1);
-			if (region.contains(dn)) {
-				board.setStateJ(s, Direction.DN, Board.UNKNOWN);
-			}
-			if (region.contains(rt)) {
-				board.setStateJ(s, Direction.RT, Board.UNKNOWN);
+			Address dn = Address.nextCell(s, Direction.DN);
+			Address rt = Address.nextCell(s, Direction.RT);
+			Address dnrt = Address.nextCell(dn, Direction.RT);
+			for (int n : Direction.DN_RT) {
+				if (region.contains(Address.nextCell(s, n))) {
+					board.setStateJ(s, n, Board.UNKNOWN);
+				}
 			}
 			if (region.containsAll(dn, rt, dnrt)) {
 				board.setNumber(s, Board.NONUMBER);
--- src/pencilbox/yajilin/BoardCopier.java
+++ src/pencilbox/yajilin/BoardCopier.java
@@ -18,12 +18,10 @@ public class BoardCopier extends BoardCopierBase {
 		Board d = (Board) dst;
 		Rotator rotator = new Rotator(src.getSize(), n);
 		rotator.rotateArrayInt3(s.getState(), d.getState());
-		Address pos0 = Address.address();
-		Address pos;
 		for (int r = 0; r < s.rows(); r++) {
 			for (int c = 0; c < s.cols(); c++) {
-				pos0.set(r, c);
-				pos = rotator.rotateAddress(pos0);
+				Address pos0 = Address.address(r, c);
+				Address pos = rotator.rotateAddress(pos0);
 				if (!d.isOn(pos))
 					continue;
 				if (s.getNumber(pos0) >= 0) {
@@ -39,27 +37,18 @@ public class BoardCopier extends BoardCopierBase {
 	public void copyRegion(BoardBase srcBoardBase, BoardBase dstBoardBase, Area region, Address from, Address to, int rotation) {
 		Board srcBoard = (Board) srcBoardBase;
 		Board board = (Board) dstBoardBase;
-		Address dn = Address.address();
-		Address rt = Address.address();
-		int joint;
-		int number;
-		int dir;
 		for (Address s : region) {
 			Address d = translateAndRotateAddress(s, from, to, rotation);
-			dn.set(s.r()+1, s.c());
-			rt.set(s.r(), s.c()+1);
-			if (region.contains(dn)) {
-				joint = srcBoard.getStateJ(s, Direction.DN);
-				dir = Rotator2.rotateDirection(Direction.DN, rotation);
-				board.setStateJ(d, dir, joint);
-			}
-			if (region.contains(rt)) {
-				joint = srcBoard.getStateJ(s, Direction.RT);
-				dir = Rotator2.rotateDirection(Direction.RT, rotation);
-				board.setStateJ(d, dir, joint);
+			for (int n : Direction.DN_RT) {
+				if (region.contains(s.nextCell(n))) {
+					int joint = srcBoard.getStateJ(s, n);
+					int dir = Rotator2.rotateDirection(n, rotation);
+					if (board.isSideOn(d, dir))
+						board.setStateJ(d, dir, joint);
+				}
 			}
 			if (board.isOn(d)) {
-				number = srcBoard.getNumber(s);
+				int number = srcBoard.getNumber(s);
 				if (number >= 0 || number == Board.BLACK) {
 					board.eraseLinesAround(d);
 				}
@@ -72,16 +61,11 @@ public class BoardCopier extends BoardCopierBase {
 
 	public void eraseRegion(BoardBase srcBoardBase, Area region) {
 		Board board = (Board) srcBoardBase;
-		Address dn = Address.address();
-		Address rt = Address.address();
 		for (Address s : region) {
-			dn.set(s.r()+1, s.c());
-			rt.set(s.r(), s.c()+1);
-			if (region.contains(dn)) {
-				board.setStateJ(s, Direction.DN, Board.UNKNOWN);
-			}
-			if (region.contains(rt)) {
-				board.setStateJ(s, Direction.RT, Board.UNKNOWN);
+			for (int n : Direction.DN_RT) {
+				if (region.contains(Address.nextCell(s, n))) {
+					board.setStateJ(s, n, Board.UNKNOWN);
+				}
 			}
 			board.setNumber(s, Board.BLANK);
 		}
