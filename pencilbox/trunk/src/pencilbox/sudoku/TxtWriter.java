a9dab240f244b78a5b2c021c2c880bcc7dcb87ed
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2010-05-21 13:45:23 +0000

    SD: enable to undo problem edit.

--- pencilbox/trunk/src/pencilbox/sudoku/TxtWriter.java
+++ pencilbox/trunk/src/pencilbox/sudoku/TxtWriter.java
@@ -19,6 +19,9 @@ public class TxtWriter extends TxtWriterBase {
 		for(int r=0; r<board.rows(); r++){
 			for(int c=0; c<board.cols(); c++){
 				if (board.isStable(r,c)) {
+					int n = board.getNumber(r,c);
+					if (n == Board.UNDETERMINED)
+						n = 0;
 					out.print(board.getNumber(r,c));
 				} else {
 					out.print('.');
