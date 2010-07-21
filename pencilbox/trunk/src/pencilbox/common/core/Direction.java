42fc5b0f5b1bafb9bb603b1f86937ff91450ec39
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2010-07-21 15:31:10 +0000

    Add diagonal direction to Direction constants.

--- pencilbox/trunk/src/pencilbox/common/core/Direction.java
+++ pencilbox/trunk/src/pencilbox/common/core/Direction.java
@@ -18,7 +18,13 @@ public class Direction {
 	public static final int DN = 2;
 	public static final int RT = 3;
 
+	public static final int LTUP = 4;
+	public static final int LTDN = 5;
+	public static final int RTDN = 6;
+	public static final int RTUP = 7;
+
 	public static final int[] UP_LT_DN_RT = {UP, LT, DN, RT};
+	public static final int[] DIAGONAL4 = {LTUP, LTDN, RTDN, RTUP};
 	public static final int[] DN_RT = {DN, RT};
 
 }
