8e4fae70135a0e75a37f5e0f762a26c58e09f364
knayuta <knayuta@6e02bbb8-f690-4ede-bbbb-3fcb4cdd5abe>
2010-07-31 02:02:31 +0000

    do not use Board.addArea method in PzprReader.

--- pencilbox/trunk/src/pencilbox/hakyukoka/PzprReader.java
+++ pencilbox/trunk/src/pencilbox/hakyukoka/PzprReader.java
@@ -39,7 +39,7 @@ public class PzprReader extends PzprReaderBase {
 		Area[] areaArray = new Area[nArea];
 		for (int k=0; k<nArea; k++) {
 			areaArray[k] = new Area();
-			bd.addArea(areaArray[k]);
+			bd.getAreaList().add(areaArray[k]);
 		}
 		for (int i = 0; i < bd.rows()*bd.cols(); i++) {
 			int k = areaIds[i]-1;
